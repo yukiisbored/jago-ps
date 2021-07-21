6481d1dbde8389b7421a0a66e631a633c9ba49cc
Muhammad Kaisar Arkhan (Yuki) <hi@yukiisbo.red>
2021-07-21 23:43:08 +0200

    Port Html AST and compiler to PureScript

--- spago.dhall
+++ spago.dhall
@@ -1,5 +1,6 @@
 { name = "jago"
-, dependencies = [ "console", "effect", "prelude", "psci-support" ]
+, dependencies =
+  [ "console", "effect", "foldable-traversable", "prelude", "psci-support" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
---
+++ src/Jago/Html.purs
@@ -0,0 +1,47 @@
+-- |
+-- This module contains the intermediate generalized representation for
+-- HTML and functions that is able to construct an HTML document from
+-- the representation.
+module Jago.Html
+       ( HtmlAttribute
+       , Html
+       , compileDocument
+       ) where
+
+import Prelude
+
+import Data.Foldable (class Foldable, foldr, intercalate)
+
+-- | Representation of an HTML attribute
+data HtmlAttribute = HtmlAttribute String String
+
+-- | Representation of a HTML tag or a text literal
+data Html = HtmlTag String (Array HtmlAttribute) (Array Html)
+          | HtmlLiteral String
+
+-- | Fold a list using the monoid
+mconcat :: forall f a. (Foldable f) => (Semigroup a) => (Monoid a) => f a -> a
+mconcat = foldr append mempty
+
+-- | Compile HtmlAttribute representation to HTML attribute
+compileAttribute :: HtmlAttribute -> String
+compileAttribute (HtmlAttribute k v) = mconcat [k, "=", "\"", v, "\""]
+
+-- | Compile an array of HtmlAttribute representations to HTML attributes
+compileAttributes :: Array HtmlAttribute -> String
+compileAttributes = intercalate " " <<< map compileAttribute
+
+-- | Compile a Html representation to HTML
+compileHtml :: Html -> String
+compileHtml (HtmlLiteral s) = s
+compileHtml (HtmlTag tagName attrs child) = mconcat [start, child', end]
+  where start = case attrs of
+          [] -> mconcat ["<", tagName, ">"]
+          xs -> mconcat ["<", tagName, " ", compileAttributes xs, ">"]
+        end = mconcat ["</", tagName, ">"]
+        child' = mconcat $ map compileHtml child
+
+-- | Compile a Html document representation to HTML
+compileDocument :: Array Html -> String
+compileDocument doc = mconcat [doc', "\n"]
+  where doc' = mconcat $ map compileHtml doc
